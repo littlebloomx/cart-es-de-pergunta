7e11788f7f51251ac9489cc033264bc00695ae7d
maques157 <147673100+littlebloomx@users.noreply.github.com>
2024-12-02 01:44:34 -0300

    projeto finalizado

---
+++ perguntas.js
@@ -0,0 +1,56 @@
+criaCartao(
+   "ESPORTE", 
+   "oq a posição de ponteiro faz no vôlei", 
+   "é uma posição do voleibol que exige do jogador a capacidade de recepcionar, defender e atacar"
+);
+
+criaCartao(
+    "Games", 
+    "qual principal objetivo do valorant?", 
+    "armar a Spike, uma espécie de bomba pela parte dos atacantes, e defusar essa spike pela parte dos defensores"
+ );
+
+ criaCartao(
+    "Música", 
+    "Quem foi o criador do trap Brasileiro?", 
+    "RAFFA MOREIRA"
+ );
+
+ criaCartao(
+    "Games", 
+    "qual a função de um duelista no valoran?t", 
+    "Eles são responsáveis por abrir caminho para a equipe e causam danos em grandes áreas e mais agressivos."
+ );
+ 
+ criaCartao(
+    "ESPORTE", 
+    "no tênis de mesa quantos cm precisa joga a bolinha para sacar?", 
+    "pelo menos 16 cm de altura para que o saque seja válido"
+ );
+ 
+ criaCartao(
+    "Música", 
+    "pra que serve o autotune?", 
+    "é um processador de áudio que corrige e afina notas vocais e instrumentais, permitindo que os artistas produzam músicas com mais precisão"
+ );
+
+ criaCartao(
+    "Games", 
+    "qual melhor jogador de valorant do mundo?", 
+    "Atualmente o melhor jogador do mundo de valorant é o Aspas"
+ );
+
+ criaCartao(
+    "ESPORTE", 
+    "qual posição mais importante do vôlei", 
+    "A posição de levantador"
+ );
+
+ criaCartao(
+    "Música", 
+    "O que é uma mixagem", 
+    " é o processo de combinar faixas gravadas de áudio em um ou mais canais, com o objetivo de criar um equilíbrio entre os elementos da música"
+ );
+
+
+ 
